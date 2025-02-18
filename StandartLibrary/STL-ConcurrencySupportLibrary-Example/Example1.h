f492833bb672713a65acccd02f7af736dd5ed7dd
Sergey Tyulenev <sergey.tyulenev@avroid.tech>
2025-02-18 11:11:36 +0300

    Added examples

--- StandartLibrary/STL-ConcurrencySupportLibrary-Example/Example1.h
+++ StandartLibrary/STL-ConcurrencySupportLibrary-Example/Example1.h
@@ -23,7 +23,7 @@
 
 namespace Example1 {
 
-namespace T1 { // ------------------------------------ Умный указатель с уникальной семантикой владения объектом (unique_ptr)
+namespace T1 { // ------------------------------------ thread
 
 void func(int n)
 {
@@ -40,7 +40,8 @@ void test_1()
 
     // ...
 
-    t.join();
+    if (t.joinable())
+        t.join();
 }
 
 // Пример с функцией в параллельном потоке (без ожидания потока)
@@ -48,22 +49,123 @@ void test_2()
 {
     std::thread t(func, 5);
 
-    std::cout << "Thread id (test_1): " << std::this_thread::get_id() << std::endl;
+    std::cout << "Thread id (test_2): " << std::this_thread::get_id() << std::endl;
 
     // ...
 
-    t.detach();
+    if (t.joinable())
+        t.detach();
 
     // В потоке произойдёт std::terminate, если t будет уничтожено раньше завершения вычисления
     //std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Задержка
+}
+
+// Пример с функцией в параллельном потоке (с joinable == true)
+void test_3()
+{
+    std::thread t(func, 5);
+
+    std::cout << "Thread id (test_3): " << std::this_thread::get_id() << std::endl;
+
+    // Будет std::terminate, т.к. ни join, ни detach не были вызваны.
 }
 
 void test()
 {
     test_1();
     test_2();
+    //test_3();
 }
 
 } // namespace T1
+
+
+
+namespace T3 { // ------------------------------------ yield
+
+void func(bool status, int id)
+{
+    if (status) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    } else {
+        // Здесь поток приостановится и предоставит приоритет другим
+        // на небольшой промежуток времени
+        std::this_thread::yield();
+    }
+
+     std::cout << "Thread id (func): " << id << std::endl;
+}
+
+void test()
+{
+    std::thread t1(func, true,  1);
+    std::thread t2(func, false, 2);
+    std::thread t3(func, true,  3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+}
+
+} // namespace T3
+
+
+
+namespace T4 { // ------------------------------------ get_id
+
+void func()
+{
+    // Выводит id потока
+    std::cout << "Thread id (func): " << std::this_thread::get_id() << std::endl;
+}
+
+void test()
+{
+    std::thread t1(func);
+    std::thread t2(func);
+    std::thread t3(func);
+
+    t1.join();
+    t2.join();
+    t3.join();
+}
+
+} // namespace T4
+
+
+
+namespace T5 { // ------------------------------------ sleep_for
+
+void func()
+{
+    // Блокировка выполнения потока на 1000 мс
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+}
+
+void test()
+{
+    std::thread t(func);
+    t.join();
+}
+
+} // namespace T5
+
+
+
+namespace T6 { // ------------------------------------ sleep_until
+
+void func()
+{
+    // Блокировка выполнения потока до определенного времени (текущее время + 1000 мс)
+    std::this_thread::sleep_until( std::chrono::steady_clock::now() + std::chrono::milliseconds(1000));
+}
+
+void test()
+{
+    std::thread t(func);
+    t.join();
+}
+
+} // namespace T6
 
 } // namespace Example1
---
+++ StandartLibrary/STL-ConcurrencySupportLibrary-Example/Example5.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <iostream>
+#include <thread>
+
+/*
+ * =======================================================================================================================================================================
+ *                                                                           Mutual exclusion
+ * =======================================================================================================================================================================
+ *
+ * Mutual exclusion:
+ * - mutex
+ * - timed_mutex
+ * - recursive_mutex
+ * - recursive_timed_mutex
+ * - shared_mutex
+ * - shared_timed_mutex
+ *
+ * Generic mutex management
+ * - lock_guard
+ * - scoped_lock
+ * - unique_lock
+ * - shared_lock
+ * - defer_lock
+ * - try_to_lock
+ * - adopt_lock
+ * - defer_lock_t
+ * - try_to_lock_t
+ * - adopt_lock_t
+ *
+ * Generic locking algorithms
+ * - try_lock
+ * - lock
+ *
+ * Call once
+ * - once_flag
+ * - call_once
+ *
+ */
+
+namespace Example5 {
+
+
+} // namespace Example5
