7df9d256c8c9567793afdcb5529d7a23502f1c09
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-02-18 11:11:36 +0300

    Added examples

--- StandartLibrary/STL-ConcurrencySupportLibrary-Example/main.cpp
+++ StandartLibrary/STL-ConcurrencySupportLibrary-Example/main.cpp
@@ -3,7 +3,11 @@
 int main()
 {
     {
-        Example1::T1::test();
+        //Example1::T1::test();
+        //Example1::T3::test();
+        //Example1::T4::test();
+        //Example1::T5::test();
+        Example1::T6::test();
     }
 
     return 0;
