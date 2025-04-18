5ac4072ce80c804791e4b4f4542f2cd6f9344d41
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-04-18 13:31:06 +0300

    Added PointerErrors-Example

---
+++ Errors/PointerErrors-Example/main.cpp
@@ -0,0 +1,15 @@
+#include "Example1.h"
+
+int main()
+{
+    {
+        // Example1::test1();
+        // Example1::test2();
+        // Example1::test3();
+        // Example1::test4();
+        // Example1::test5();
+        // Example1::test6();
+    }
+
+    return 0;
+}
