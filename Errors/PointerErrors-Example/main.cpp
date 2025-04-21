7f8d3636140f96960f856fc7efee6d5aaa52abf8
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-04-21 12:10:21 +0300

    Added examples

--- Errors/PointerErrors-Example/main.cpp
+++ Errors/PointerErrors-Example/main.cpp
@@ -1,4 +1,5 @@
 #include "Example1.h"
+#include "Example2.h"
 
 int main()
 {
@@ -9,6 +10,17 @@ int main()
         // Example1::test4();
         // Example1::test5();
         // Example1::test6();
+    }
+
+    {
+        // Example2::test1();
+        // Example2::test2();
+        // Example2::test3();
+        // Example2::test4();
+        // Example2::test5();
+        // Example2::test6();
+        // Example2::test7();
+        Example2::test8();
     }
 
     return 0;
