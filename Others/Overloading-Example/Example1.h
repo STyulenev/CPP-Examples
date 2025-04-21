b85155a5086fb52b46ed270dd0351f0dea9b2ce2
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-04-21 15:04:03 +0300

    Added examples

--- Others/Overloading-Example/Example1.h
+++ Others/Overloading-Example/Example1.h
@@ -3,7 +3,18 @@
 #include <iostream>
 
 /*
- * Примеры всевозможных перегрузок метода класса
+ * Примеры всевозможных перегрузок метода класса:
+ *
+ * - Для rvalue и lvalue объектов;
+ * - Для const и не const объектов;
+ * - Для volatile и не volatile объектов;
+ * - Для const volatile и не const volatile объектов;
+ * - Для rvalue и lvalue объектов + типизация шаблона;
+ * - Для const и не const объектов + типизация шаблона;
+ * - Для volatile и не volatile объектов + типизация шаблона;
+ * - Для const volatile и не const volatile объектов + типизация шаблона.
+ * - Различное количество аргументов.
+ * - Различные типы аргументов.
  *
  */
 
@@ -70,6 +81,46 @@ struct SomeClass {
     // void foo(const volatile int x) {} // Ошибка, это равнозначно т.к. const volatile удаляется из-за передачи по значению
     // long long int foo(int x) {}       // Ошибка, нельзя перегружать по возвращаемому значению
 
+    template<typename T>
+    void foo() & {
+        std::cout << "void foo<" << typeid(T).name() << ">() &" << std::endl;
+    }
+
+    template<typename T>
+    void foo() && {
+        std::cout << "void foo<" << typeid(T).name() << ">() &&" << std::endl;
+    }
+
+    template<typename T>
+    void foo() const & {
+        std::cout << "void foo<" << typeid(T).name() << ">() const &" << std::endl;
+    }
+
+    template<typename T>
+    void foo() const && {
+        std::cout << "void foo<" << typeid(T).name() << ">() const &&" << std::endl;
+    }
+
+    template<typename T>
+    void foo() volatile & {
+        std::cout << "void foo<" << typeid(T).name() << ">() volatile &" << std::endl;
+    }
+
+    template<typename T>
+    void foo() volatile && {
+        std::cout << "void foo<" << typeid(T).name() << ">() volatile &&" << std::endl;
+    }
+
+    template<typename T>
+    void foo() const volatile & {
+        std::cout << "void foo<" << typeid(T).name() << ">() const volatile &" << std::endl;
+    }
+
+    template<typename T>
+    void foo() const volatile && {
+        std::cout << "void foo<" << typeid(T).name() << ">() const volatile &&" << std::endl;
+    }
+
 };
 
 void test()
@@ -109,6 +160,46 @@ void test()
     {
         SomeClass* value = new SomeClass();
         value->foo(); // void foo() &
+        delete value;
+    }
+
+    std::cout << std::endl;
+
+    {
+        SomeClass lvalue;
+        lvalue.foo<int>();      // void foo<i>() &
+        SomeClass{}.foo<int>(); // void foo<i>() &&
+    }
+
+    std::cout << std::endl;
+
+    {
+        const SomeClass lvalue;
+        lvalue.foo<int>();               // void foo<i>() const &
+        SomeClass::createC().foo<int>(); // void foo<i>() const &&
+    }
+
+    std::cout << std::endl;
+
+    {
+        volatile SomeClass lvalue;
+        lvalue.foo<int>();               // void foo<i>() volatile &
+        SomeClass::createV().foo<int>(); // void foo<i>() volatile &&
+    }
+
+    std::cout << std::endl;
+
+    {
+        const volatile SomeClass lvalue;
+        lvalue.foo<int>();                // void foo<i>() const volatile &
+        SomeClass::createCV().foo<int>(); // void foo<i>() const volatile &&
+    }
+
+    std::cout << std::endl;
+
+    {
+        SomeClass* value = new SomeClass();
+        value->foo<int>(); // void foo<i>() &
         delete value;
     }
 }
