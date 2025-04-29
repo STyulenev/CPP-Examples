f382b9c75e08a112d612b1afbe031a1f8dd287cd
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-04-29 15:34:07 +0300

    Added Inline-Example

---
+++ Others/Inline-Example/Example1.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <iostream>
+
+// Избежали многократного определения в Example1A.cpp и Example1B.cpp
+inline int add(int a, int b) {
+    return a + b;
+}
+
+// Избежали многократного определения в Example1A.cpp и Example1B.cpp
+template <typename T>
+inline T findMax(T a, T b) {
+    return (a > b) ? a : b;
+}
+
+// Избежали многократного определения в Example1A.cpp и Example1B.cpp
+inline const double PI = 3.14;
+
+// Для оптимизации компилятором простых функций (делать их встроенными), но после C++98 компилятору уже не нужны такие подсказки.
+inline int square(int x) {
+    return x * x;
+}
+
+// Создание методов классов за его пределами
+class MyClass
+{
+public:
+    int getField() const;
+    void setField(int newField);
+
+private:
+    int field;
+
+};
+
+inline int MyClass::getField() const
+{
+    return field;
+}
+
+inline void MyClass::setField(int newField)
+{
+    field = newField;
+}
+
+namespace Example1 {
+
+void test1();
+void test2();
+
+inline void test3() // Избежали многократного определения
+{
+    std::cout << "\ntest3()\n\n";
+
+    std::cout << "2 * 2 = " << square(2) << std::endl;
+}
+
+inline void test4() // Избежали многократного определения
+{
+    std::cout << "\ntest4()\n\n";
+
+    MyClass myClass;
+    myClass.setField(5);
+
+    std::cout << "myClass.field = " << myClass.getField() << std::endl;
+}
+
+} // namespace Example1
---
+++ Others/Inline-Example/Example1B.cpp
@@ -0,0 +1,14 @@
+#include "Example1.h"
+
+namespace Example1 {
+
+void test2()
+{
+    std::cout << "\ntest2()\n\n";
+
+    std::cout << "1 + 2 = " << add(1, 2) << std::endl;
+    std::cout << "1 or 2 = " << findMax<int>(1, 2) << std::endl;
+    std::cout << "PI = " << PI << std::endl;
+}
+
+} // namespace Example1
