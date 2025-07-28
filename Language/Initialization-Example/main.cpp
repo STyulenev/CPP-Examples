f7af44a1f2a59cd85eaefa669410eef358f2dcd7
Sergey Tyulenev <tyulenev.i@yandex.ru>
2025-07-28 10:52:11 +0300

    Added examples

--- Language/Initialization-Example/main.cpp
+++ Language/Initialization-Example/main.cpp
@@ -1,12 +1,15 @@
 #include "Example1.h"
 #include "Example2.h"
+#include "Example3.h"
 
 int main()
 {
     //Example1::test();
 
-    Example2::test1();
-    Example2::test2();
+    //Example2::test1();
+    //Example2::test2();
+
+    Example3::test1();
 
     return 0;
 }
